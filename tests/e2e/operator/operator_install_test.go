0625f93d158299a2b4a1fced288b8f2d1883ac85
mkralik3 <mkralik@redhat.com>
2026-08-24 10:04:48 +0200

    Fixes after rebase
    ssia
    
    Signed-off-by: mkralik3 <mkralik@redhat.com>

--- tests/e2e/operator/operator_install_test.go
+++ tests/e2e/operator/operator_install_test.go
@@ -41,6 +41,7 @@ import (
 	. "github.com/onsi/gomega"
 	configv1 "github.com/openshift/api/config/v1"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
